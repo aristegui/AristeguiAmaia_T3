95a2b36fd0e0c9cc18a2d0ff712cb8259716a1ff
aristegui <amaia.aristegui@gmail.com>
2021-01-02 14:12:20 +0100

    Update LogOut.php

--- php/LogOut.php
+++ php/LogOut.php
@@ -1,5 +1,5 @@
 <?php
 
-	echo("<script>alert('¡HASTA PRONTO ¡$mail!')</script>");
+	echo("<script>alert('¡HASTA PRONTO!')</script>");
     echo("<script>window.location = 'Layout.php';</script>");
 ?>
